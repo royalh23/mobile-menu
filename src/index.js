e42b9754fa5f92d7447f3dc18ecb3798ae3490b2
Royal <royalhuseyn902@gmail.com>
2024-01-02 22:41:30 +0400

    Bundle all JS code inside IIFE

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
 import './style.css';
 
-const content = document.querySelector('#content');
-const more = document.querySelector('.more');
-const moreItems = document.querySelector('.more-items');
+(() => {
+  const content = document.querySelector('#content');
+  const more = document.querySelector('.more');
+  const moreItems = document.querySelector('.more-items');
 
-function closeMoreItems() {
-  if (moreItems.classList.contains('visible')) {
+  function closeMoreItems() {
+    if (moreItems.classList.contains('visible')) {
+      moreItems.classList.toggle('visible');
+    }
+  }
+
+  function showMoreItems(e) {
+    e.stopPropagation();
     moreItems.classList.toggle('visible');
   }
-}
 
-function showMoreItems(e) {
-  e.stopPropagation();
-  moreItems.classList.toggle('visible');
-}
-
-content.addEventListener('click', closeMoreItems);
-more.addEventListener('click', (e) => showMoreItems(e));
+  content.addEventListener('click', closeMoreItems);
+  more.addEventListener('click', (e) => showMoreItems(e));
+})();
