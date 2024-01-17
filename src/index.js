aa99812f9c1df7cdf0f5a4373ca3c8c5c72626c9
Royal <royalhuseyn902@gmail.com>
2024-01-17 22:58:00 +0400

    Remove IIFE from index.js

--- src/index.js
+++ src/index.js
@@ -1,22 +1,20 @@
 import './style.css';
 
-(() => {
-  const content = document.querySelector('#content');
-  const more = document.querySelector('.more');
-  const moreItems = document.querySelector('.more-items');
+const content = document.querySelector('#content');
+const more = document.querySelector('.more');
+const moreItems = document.querySelector('.more-items');
 
-  function closeMoreItems() {
-    if (moreItems.classList.contains('visible')) {
-      moreItems.classList.toggle('visible');
-    }
-  }
-
-  function showMoreItems(e) {
-    e.stopPropagation();
+function closeMoreItems() {
+  if (moreItems.classList.contains('visible')) {
     moreItems.classList.toggle('visible');
   }
+}
 
-  content.addEventListener('click', closeMoreItems);
-  more.addEventListener('click', (e) => showMoreItems(e));
-  moreItems.addEventListener('click', (e) => e.stopPropagation());
-})();
+function showMoreItems(e) {
+  e.stopPropagation();
+  moreItems.classList.toggle('visible');
+}
+
+content.addEventListener('click', closeMoreItems);
+more.addEventListener('click', (e) => showMoreItems(e));
+moreItems.addEventListener('click', (e) => e.stopPropagation());
